cbad476ff6983cb74166a58d59a4ddc091df3e37
Robert Slocumbe <rslocumbe93@gmail.com>
2022-04-25 19:28:23 -0400

    fixed syntax errors in .sql files

--- db/schema.sql
+++ db/schema.sql
@@ -4,28 +4,25 @@ CREATE DATABASE company_db;
 USE company_db;
 
 CREATE TABLE department (
-    id: INT NOT NULL,
-    name: VARCHAR(30),
-    PRIMARY KEY (id)
+    id INT NOT NULL PRIMARY KEY,
+    name VARCHAR(30)
 );
 
-CREATE TABLE role (
-    id: INT NOT NULL,
-    title: VARCHAR(30),
-    salary: DECIMAL,
-    department_id: INT, --References TABLE:department KEY:id id--
-    PRIMARY KEY (id)
+CREATE TABLE roles (
+    id INT NOT NULL PRIMARY KEY,
+    title VARCHAR(30),
+    salary DECIMAL,
+    department_id INT, 
     FOREIGN KEY (department_id)
     REFERENCES department(id)
 );
 
-CREATE TABLE employee (
-    id: INT NOT NULL,
-    first_name: VARCHAR(30),
-    last_name: VARCHAR(30),
-    role_id: INT, --References TABLE:role KEY:id--
-    manager_id: INT, --TO HOLD REFERENCE TO ANOTHER EMPLOYEE THAT IS THE MANAGER OF THE CURRENT EMPLOYEE (NULL IF THE EMPLOYEE HAS NO MANAGER)
-    PRIMARY KEY (id)
+CREATE TABLE employees (
+    id INT NOT NULL PRIMARY KEY,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT, 
+    manager_id INT,
     FOREIGN KEY (role_id)
-    REFERENCES role(id)
+    REFERENCES roles(id)
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -4,7 +4,7 @@ VALUES  (001, "Sales"),
         (003, "Finance"),
         (004, "Legal");
 
-INSERT INTO role (id, title, salary, department_id)
+INSERT INTO roles (id, title, salary, department_id)
 VALUES  (001, "Sales Lead", 100000, 001),
         (002, "Salesperson", 80000, 001),
         (003, "Lead Engineer", 150000, 002),
@@ -14,12 +14,12 @@ VALUES  (001, "Sales Lead", 100000, 001),
         (007, "Legal Team Lead", 250000, 004),
         (008, "Lawyer", 190000, 004);
 
-INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
-VALUES  (001, "Shane", "Patel", 001),
+INSERT INTO employees (id, first_name, last_name, role_id, manager_id)
+VALUES  (001, "Shane", "Patel", 001, NULL),
         (002, "Beth", "Streible", 002, 001),
-        (003, "Robert", "Slocumbe", 003),
+        (003, "Robert", "Slocumbe", 003, NULL),
         (004, "Natalie", "Ray", 004, 003),
-        (005, "Christopher", "Marbut", 005),
+        (005, "Christopher", "Marbut", 005, NULL),
         (006, "Lotta", "Milde", 006, 005),
-        (007, "Jason", "Bennett", 007),
-        (008, "Courtney", "Watts", 008, 007),
+        (007, "Jason", "Bennett", 007, NULL),
+        (008, "Courtney", "Watts", 008, 007);
